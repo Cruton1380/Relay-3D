b24085539817e6a2d8a2701478344d6bf93e175a
Eitan Asulin <eitana@ccr_net.local>
2026-01-26 23:40:25 +0200

    chore: massive root directory cleanup - archive 251 status docs, organize scripts

--- relay-main/apps/client-react-native/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Component exports
- */
-
-export { default as PeersView } from './PeersView';
-export { default as RepoTab } from './RepoTab';
-export { default as MarkdownRenderer } from './MarkdownRenderer';
-export { default as ScriptConsole } from './ScriptConsole';
-export { default as DebugTab } from './DebugTab';
--- relay-main/build-relay.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd /d "%~dp0"
-cargo build --release --bin relay-server
-
-
-
-
-
